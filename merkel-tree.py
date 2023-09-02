79d1deb8423c0732f9bf8b08f466b2a79d3e979d
Zed <gyana.cse7@gmail.com>
2023-09-02 13:52:18 +0530

    first comit

---
+++ merkel-tree.py
@@ -0,0 +1,34 @@
+import hashlib
+
+# Define a function to calculate the hash of a data element
+def calculate_hash(data):
+    return hashlib.sha256(data.encode()).hexdigest()
+
+# List of data elements (transactions)
+data_list = ["Transaction1", "Transaction2", "Transaction3", "Transaction4"]
+
+# Step 1: Calculate the hash for each data element and store them in a list
+hashed_data_list = [calculate_hash(data) for data in data_list]
+
+# Step 2: Create leaf nodes of the Merkle tree
+leaf_nodes = hashed_data_list
+
+# Step 3: Build the Merkle tree from the leaf nodes
+while len(leaf_nodes) > 1:
+    new_level = []
+    # Pair adjacent nodes, hash them together, and store the result in the new level
+    for i in range(0, len(leaf_nodes), 2):
+        if i + 1 < len(leaf_nodes):
+            combined_hash = calculate_hash(leaf_nodes[i] + leaf_nodes[i + 1])
+            new_level.append(combined_hash)
+        else:
+            # If there's an odd number of nodes, hash the last node with itself
+            combined_hash = calculate_hash(leaf_nodes[i] + leaf_nodes[i])
+            new_level.append(combined_hash)
+    leaf_nodes = new_level
+
+# Step 4: The root hash (Merkle root) is the final remaining element in the tree
+merkle_root = leaf_nodes[0]
+
+# Print the Merkle root
+print("Merkle Root:", merkle_root)
